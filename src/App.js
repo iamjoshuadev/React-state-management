0c771eb5bb302e82ac15d07259f570327651986b
Joshua <joshuapenacemas@gmail.com>
2023-05-18 21:37:38 -0400

    encontrado

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import TodoApp from "./components/todoApp";
+
+function App() {
+  return (
+    <div className="continer">
+      
+      <h1>Tareas</h1>
+    <TodoApp/>
+    </div>
+  );
+}
+
+export default App;
